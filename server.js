db28560ad57eabd3d44d6bf7db744754ba9acfb1
Ebere <eiweala@live.com>
2024-04-20 18:36:57 -0700

    Code fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let http = require('http');
 
 http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': text/plain});
+    response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello Node!\n');
 }).listen(8080);
 
